e11a5828e70d4028a524c33d8449697ba39bfd94
Bob_work <musgravehill@gmail.com>
2019-03-13 14:21:13 +0300

    freq gen

---
+++ FreqGen/FreqGen.ino
@@ -0,0 +1,14 @@
+#define _BV(bit) (1<< (bit))
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  PORTB |= _BV(PB5); // HIGH
+  //delayMicroseconds(10);
+  delay(30);
+  PORTB &= ~ _BV(PB5); // LOW
+  //delayMicroseconds(10);
+  delay(30);
+}
